e99187472b6b63bba43bd4168af27f0c5ec53097
Pablo Andres Guzman Alarcon <123975198+Pableis05@users.noreply.github.com>
2024-09-13 17:30:39 -0500

    issue: organization repo

--- src/flowlance_virtual_environment/Scripts/activate.bat
+++ src/flowlance_virtual_environment/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ASUS\Desktop\Proyecto Integrador\202402-proyecto-gecko-team\src\flowlance_virtual_environment
+set VIRTUAL_ENV=D:\Semestre 5\202402-proyecto-gecko-team\src\flowlance_virtual_environment
 
 if not defined PROMPT set PROMPT=$P$G
 
