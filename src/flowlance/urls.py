8d03003e31fa73ba6cdc59db7130cb95bd386b61
JuanJDlp <juanjosedelapava1@gmail.com>
2024-09-15 18:53:44 -0500

    feat: Added the calendar style and made it a static file

--- src/flowlance/urls.py
+++ src/flowlance/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     path("project/", include("project_controller.urls")),  # Incluye URLs de control de proyectos
     path('profile/', include('freelancer_profile_creation.urls')),  # Incluye URLs de creación de perfiles
     path('dashboard/', include('dashboard.urls')),  # Incluye URLs del dashboard
-    path('admin/', admin.site.urls),
     path('time_line/', include('time_line.urls')),  # Incluye las URLs de time_line
 ]
 
