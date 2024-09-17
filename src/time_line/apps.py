62e671454829b4374abf186c0887cae03ca8ef33
Rony Ordoñez <ronyfarido@gmail.com>
2024-09-16 22:03:17 -0500

    FIX: Deleted unnecesary app created by Pablo Parra c:

--- src/time_line/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TimeLineConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'time_line'
--- src/time_line/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('calificar/<int:freelancer_id>/', views.calificar_freelancer, name='calificar_freelancer'),
-
-    # La ruta es asi debido a la logica anterior donde el cliente le tiene que dar click en la pantalla a que freelancer quiere calificar
-]
