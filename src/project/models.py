989a5248e6a56f50232cf5984d89c024e318fa25
Juan Jose De La Pava <123976319+JuanJDlp@users.noreply.github.com>
2024-09-19 17:50:07 -0500

    Update models.py

--- src/project/models.py
+++ src/project/models.py
@@ -17,21 +17,6 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
-    
-class ProjectAvailable(models.Model):
-    id = models.AutoField(primary_key=True)
-    image = models.CharField(blank=True, max_length=500)
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    requirements = models.TextField()
-    budget = models.DecimalField(max_digits=10, decimal_places=2)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    client = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects_available")
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
 
 
 class Milestone(models.Model):
