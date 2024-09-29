f5de6052daec29ee1ce64bbc4478cc2e68c400ee
Rony Ordoñez <ronyfarido@gmail.com>
2024-09-28 20:50:34 -0500

    feat: Refactor decorators and add user type checks

--- src/flowlance/decorators.py
+++ src/flowlance/decorators.py
@@ -1,5 +1,7 @@
 from functools import wraps
 
+from django.shortcuts import render # temporal render
+
 def attach_profile_info(func):
     @wraps(func)
     def wrapper(request, *args, **kwargs):
@@ -8,4 +10,21 @@ def attach_profile_info(func):
         request.profile = profile
         request.profile_type = profile_type
         return func(request, *args, **kwargs)
+    return wrapper
+
+def freelancer_required(func):
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        if user_type == 'freelancer':
+            return func(request, *args, **kwargs)
+        return render(request, 'errors/error_generic.html', {'error_type':'custom_permission'}) # temporal render
+    return wrapper
+
+def client_required(func):
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        profile, user_type = request.user.get_profile_info()
+        if user_type == 'client':
+            return func(request, *args, **kwargs)
+        return render(request, 'errors/error_generic.html', {'error_type':'custom_permission'}) # temporal render
     return wrapper
