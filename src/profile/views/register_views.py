bdb3f0ac03b76db230d996a4525319dacee0fd8e
David Artunduaga <1040871155@u.icesi.edu.co>
2024-09-29 00:09:46 -0500

    Feat: Fronted of the freelancer register and animations implemeted

--- src/profile/views/register_views.py
+++ src/profile/views/register_views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from ..forms import FreelancerRegisterForm, CompanyRegisterForm
+from django.db import IntegrityError
+
 
 
 def register_freelancer(request):
@@ -9,7 +11,7 @@ def register_freelancer(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("/dashboard/")
+            return render(request, 'profile/register_freelancer.html', {'form': form, 'registration_successful': True})
 
     else:
         form = FreelancerRegisterForm()
@@ -18,12 +20,25 @@ def register_freelancer(request):
 
 
 def register_company(request):
+    registration_successful = False
     if request.method == 'POST':
         form = CompanyRegisterForm(request.POST, request.FILES)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("/dashboard/")
+            try:
+                user = form.save()
+                login(request, user)
+                registration_successful = True
+                # No renderices el formulario nuevamente si el registro es exitoso
+                return render(request, 'profile/register_company.html', {
+                    'form': CompanyRegisterForm(),  # Limpia el formulario
+                    'registration_successful': registration_successful
+                })
+            except IntegrityError:
+                form.add_error(None, 'Error al crear el usuario, por favor intenta de nuevo.')
     else:
         form = CompanyRegisterForm()
-    return render(request, 'profile/register_company.html', {'form': form})
+    
+    return render(request, 'profile/register_company.html', {
+        'form': form,
+        'registration_successful': registration_successful
+    })
