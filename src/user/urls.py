9202ac766c009a7243114ff9132980c08584f65b
Juanpapb0401 <e5jparra@gmail.com>
2024-09-29 12:46:41 -0500

    feat: Google Authentication Login

--- src/user/urls.py
+++ src/user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login_view, name="login"),
     path('logout/', LogoutView.as_view(next_page="/"), name='logout'), 
+    
 ]
