324bba23096cabc9a0bf855a0fdf10af65c04531
David Artunduaga <1040871155@u.icesi.edu.co>
2024-10-20 09:51:46 -0500

    fix: fix bug in the event model and coverage report

--- src/project/models.py
+++ src/project/models.py
@@ -177,6 +177,8 @@ class ProjectReportSettings(models.Model):
 class UserProjectReportSettings(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     report_settings = models.ForeignKey(ProjectReportSettings, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ('user', 'report_settings')
 
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
@@ -189,6 +191,3 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        unique_together = ('user', 'report_settings')
