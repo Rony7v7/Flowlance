e7ed3b3d0ecab8a6703c91b4a49b3ae817560d87
Rony Ordoñez <ronyfarido@gmail.com>
2024-10-20 15:21:27 -0500

    feat: add support for multiple roles in role_required decorator

--- src/flowlance/decorators.py
+++ src/flowlance/decorators.py
@@ -2,7 +2,7 @@ from functools import wraps
 
 from django.shortcuts import get_object_or_404, render
 
-from project.models import Project # temporal render
+from project.models import Milestone, Project, ProjectMember, Task # temporal render
 
 def attach_profile_info(func):
     @wraps(func)
@@ -32,16 +32,41 @@ def client_required(func):
         return render(request, 'errors/error_generic.html', {'error_type':'custom_permission'}) # temporal render
     return wrapper
 
-def role_required(role):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(request, *args, **kwargs):
-            project = get_object_or_404(Project, id=kwargs['project_id'])
-            member = project.memberships.get(user=request.user)
+def role_required(roles):
+    """
+    Decorador que verifica si el usuario tiene el rol necesario en el proyecto asociado.
+    Funciona con diferentes tipos de IDs (project_id, milestone_id, task_id, etc.).
+    
+    Args:
+        roles (list): Lista de roles permitidos para la vista.
+    
+    Returns:
+        Una función que verifica si el usuario tiene el rol adecuado en el proyecto.
+    """
+    def decorator(view_func):
+        def _wrapped_view(request, *args, **kwargs):
+            project = None
+
+            # Maneja diferentes tipos de IDs para obtener el proyecto
+            if 'project_id' in kwargs:
+                project = get_object_or_404(Project, id=kwargs['project_id'])
+            elif 'milestone_id' in kwargs:
+                milestone = get_object_or_404(Milestone, id=kwargs['milestone_id'])
+                project = milestone.project 
+            elif 'task_id' in kwargs:
+                task = get_object_or_404(Task, id=kwargs['task_id'])
+                project = task.milestone.project
+            elif 'member_id' in kwargs:
+                project_member = get_object_or_404(ProjectMember, id=kwargs['member_id'])
+                project = project_member.project
+
+            # Verificar si el usuario es miembro del proyecto
+            if project:
+                project_member = ProjectMember.objects.filter(user=request.user, project=project).first()
+                if not project_member or project_member.role not in roles:
+                    return render(request, 'errors/error_generic.html', {'error_type': 'custom_permission'})  # Render temporal
             
-            if member.role == role:
-                return func(request, *args, **kwargs)
-            
-            return render(request, 'errors/error_generic.html', {'error_type':'custom_permission'}) # temporal render
-        return wrapper
+            return view_func(request, *args, **kwargs)
+
+        return _wrapped_view
     return decorator
