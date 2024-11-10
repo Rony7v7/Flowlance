f35772f9d325c44e54f16e9ba557120c6fb9d0dc
Pablo Andres Guzman Alarcon <123975198+Pableis05@users.noreply.github.com>
2024-11-10 14:42:28 -0500

    fix: footer buttons

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("account_settings", views.settings, name="account_settings"),
+    path("information", views.information, name="information"),
     path("security_settings", views.security_settings, name="security_settings"),
     path("toggle_2fa/", views.toggle_2fa, name="toggle_2fa"),
     path("toggle_notification_when_profile_visited", views.toggle_notification_when_profile_visited,name = "toggle_notification_when_profile_visited"),
