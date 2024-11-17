9bba18afe75885693568910da4a89f07a11c743a
David Artunduaga <1040871155@u.icesi.edu.co>
2024-11-16 22:50:00 -0500

    fix: fix settings

--- src/flowlance/settings.py
+++ src/flowlance/settings.py
@@ -139,21 +139,14 @@ WSGI_APPLICATION = "flowlance.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Motor de la base de datos
-#         'NAME': 'test',          # Nombre de tu base de datos PostgreSQL
-#         'USER': 'postgres',                 # Usuario de PostgreSQL
-#         'PASSWORD': 'password',        # Contraseña del usuario
-#         'HOST': 'localhost',                # Servidor de PostgreSQL
-#         'PORT': '5432',                     # Puerto de PostgreSQL (por defecto es 5432)
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Motor de la base de datos
+        'NAME': 'test',          # Nombre de tu base de datos PostgreSQL
+        'USER': 'postgres',                 # Usuario de PostgreSQL
+        'PASSWORD': 'password',        # Contraseña del usuario
+        'HOST': 'localhost',                # Servidor de PostgreSQL
+        'PORT': '5432',                     # Puerto de PostgreSQL (por defecto es 5432)
     }
 }
 
